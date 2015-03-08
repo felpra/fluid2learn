b220bfc24dc0073905fa821e9a4a26cc8772b7a3
Felipe <fpratali@gmail.com>
2015-03-08 20:01:06 -0300

    implementação do Enquirer e reparo dos problemas inicais no Statistics e na pasta origem

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -19,31 +19,74 @@ public class Enquirer implements IEnquirer
 	@Override
 	public void connect(IResponder responder)
 	{
+		
         IBaseConhecimento bc = new BaseConhecimento();
-		
-		obj = bc.recuperaObjeto("tiranossauro");
+        String[] bichos = bc.listaNomes(); // puxa base de dados de animais
+        String[] jafeita = new String[42]; // perguntas ja feitas
+        String[] resp = new String[42];   // respostas ja dadas
 
-		IDeclaracao decl = obj.primeira();
+        int i=0, j=0, k=0;
+        boolean ja=false;
+        obj = bc.recuperaObjeto(bichos[i]);
+        IDeclaracao decl = obj.primeira();
+ 
+        while (decl != null){
+			ja=false; // supoe se inicialmente que a primeira pergunta n foi feita
 		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
+       
+			
 			String pergunta = decl.getPropriedade();
 			String respostaEsperada = decl.getValor();
+			j=0; // inicializa contador das ja feitas para cada animal
+			if(k!=0) //se for a 1 iteracao, nao precisa verificar se ja ha perguntas feitas
+while(jafeita[j]!=null)          { //verifica se ja ha perguntas ate limite do vetor
+                
+					if (pergunta.equalsIgnoreCase(jafeita[j]))  {
+						ja = true; //se ja foi feita ja = true
+			break;
+					                                            }
+					else{	
+						j++;// senao incrementa j para gravar a pergunta na posicao					
+					    }
+				
+				                  }
 			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
+				
+                if(!ja){ //se nao foi feita aciona o responder para obter resposta
+				String resposta = responder.ask(pergunta);
+				resp[j] = resposta; // grava resposta
+				jafeita[j] = pergunta; // grava pergunta nas já feitas
+				if (resposta.equalsIgnoreCase(respostaEsperada))
+					decl = obj.proxima(); //se for a esperada passa para proxima
+				else{ // se nao for a esperada vai para o proximo animal
+					i++;
+					obj = bc.recuperaObjeto(bichos[i]);
+							decl = obj.primeira();
+				}
+                }
+                else{ // se ja feita, compara o resultado ja existente na variavel "jafeita"
+                
+                	if (resp[j].equalsIgnoreCase(respostaEsperada))
+    					decl = obj.proxima();
+    				else{
+    					i++;
+    					obj = bc.recuperaObjeto(bichos[i]);
+    							decl = obj.primeira();
+    							
+    				}
+                }
+			
+                
+			
+            
+                k++;
+        }
+boolean acertei = responder.finalAnswer(bichos[i]);
 		
 		if (acertei)
 			System.out.println("Oba! Acertei!");
 		else
 			System.out.println("fuem! fuem! fuem!");
-
-	}
-
+	    	
+	    }
 }
